57d7651bdb8c42090057f1fdc0c1589945f26d9f
Amaury Vallet <amaury.vallet@gmail.com>
2022-03-23 17:48:24 +0100

    rajout des validates et relations inter model

--- app/models/punk.rb
+++ app/models/punk.rb
@@ -1,3 +1,9 @@
 class Punk < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :picture, presence: true
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :punk
+  belongs_to :user
+  validates :content, length: { minimum: 20 }
 end
