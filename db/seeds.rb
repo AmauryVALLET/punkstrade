9c136a3263de0a848753d867c6e1cece3134c5f2
Amaury Vallet <amaury.vallet@gmail.com>
2022-03-23 18:02:45 +0100

    rajout des routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+
+usertest = User.create!(email: "amau@gmail.com", password: "123456")
+
+puts 'Creating punks...'
+Punk.create!({
+  name: "punk A",
+  description: "this is the first punk lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+  picture: "https://journalducoin-com.exactdn.com/app/uploads/2022/02/CryptoPunk.NFT_.5822.png?strip=all&lossy=1&quality=66&resize=418%2C419&ssl=1",
+  user: usertest
+})
+Punk.create!({
+  name: "punk B",
+  description: "this is the second punk lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+  picture: "https://www.cointribune.com/app/uploads/2021/07/Crypto-Punk.png",
+  user: usertest
+})
+puts 'Finished!'
